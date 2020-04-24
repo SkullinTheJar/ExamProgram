165c541f57ba2c1e450df4d71f833597e9c98c72
DevilishMutant <andreas.gkp@gmail.com>
2020-04-24 13:59:44 +0200

    Made first attempt at classes

--- main.py
+++ main.py
@@ -3,25 +3,47 @@ import pygame
 
 pygame.init()
 
-size = width, height = 700, 500
-speed = [2, 2]
-black = 0, 0, 0
+class Player:
+    def __init__(self, xPos, yPos, radius):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.radius = radius
+    
+    def update(self):
+        if self.xPos < 0:
+            self.xPos = 0 + self.radius
 
-screen = pygame.display.set_mode(size)
+class MainFrame:
+    def __init__(self):
+        self.size = self.width, self.height = 1300, 700
+        self.speed = [2, 2]
+        self.black = 0, 0, 0
 
-ball = pygame.image.load("lavender heart.png")
-ballrect = ball.get_rect()
+        self.screen = pygame.display.set_mode(self.size)
+        pygame.display.set_caption("INSERT TITLE")
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        # ball = pygame.image.load("lavender heart.png")                             # For Aslaks PC
+        self.ball = pygame.image.load("C:/Users/andre/OneDrive/Billeder/Wrench.png")      # For Andreas' PC
+        self.ballrect = self.ball.get_rect()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    def draw(self):
+        print("do stuff")
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+    def update(self):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
+
+            self.ballrect = self.ballrect.move(self.speed)
+            if self.ballrect.left < 0 or self.ballrect.right > self.width:
+                self.speed[0] = -self.speed[0]
+            if self.ballrect.top < 0 or self.ballrect.bottom > self.height:
+                self.speed[1] = -self.speed[1]
+
+            self.screen.fill(self.black)
+            self.screen.blit(self.ball, self.ballrect)
+            pygame.display.flip()
+
+game = MainFrame()
+game.draw()
+game.update()
