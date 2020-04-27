03edd9ac132d28f9a44f8bae75b1eef8469cbe13
DevilishMutant <andreas.gkp@gmail.com>
2020-04-27 16:57:37 +0200

    Made sprites work

--- main.py
+++ main.py
@@ -8,25 +8,26 @@ def Main():
     while 1:
         game.draw()
         game.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, coordinates):
-        super().__init__(self)
+    def __init__(self, coordinates, image):
+        pygame.sprite.Sprite.__init__(self)
         self.x, self.y = self.coordinates = coordinates
-        self.image = pygame.Surface((10, 10))
-        self.image.fill(255, 0, 255)
-        self.rect = self.image.get_rect
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
 
     def draw(self):
         pass
     
     def update(self):
-        pass
+        self.rect.move_ip(1, 1)
     
     
 class Projectile((pygame.sprite.Sprite)):
     def __init__(self, coordinates):
-        super().__init__(self)
+        pygame.sprite.Sprite().__init__(self)
         self.x, self.y = self.coordinates = coordinates
         
     def draw(self):
@@ -38,22 +39,21 @@ class Projectile((pygame.sprite.Sprite)):
 class Wall(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(self)
-        
+
     
 class Game:
     def __init__(self, size):
         self.size = size
         self.screen = pygame.display.set_mode(size)
-        self.playersGroup = pygame.sprite.Group()
-        self.player1 = Player((0, 0))
-        self.player2 = Player((0, 0))
-        self.playersGroup.add(self.player1, self.player2)
+        self.player1 = Player((650, 300), "C:/Users/andre/OneDrive - AARHUS TECH/Programmering/ExamProgram/red tank.png")
+        self.player2 = Player((300, 500), "C:/Users/andre/OneDrive - AARHUS TECH/Programmering/ExamProgram/blue tank.png")
+        self.playersGroup = pygame.sprite.Group(self.player1, self.player2)
 
     def draw(self):
-        pass
+        self.playersGroup.draw(self.screen)
         
     def update(self):
-        
+        self.playersGroup.update()
         pygame.display.flip()
         
 Main()
